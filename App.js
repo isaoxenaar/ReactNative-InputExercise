03ae8d242f60a9de6816cd035f858ee36f191948
Isa Oxenaar <isamoxenaar@gmail.com>
2020-05-28 16:14:49 +0200

    Added a working textinput field including needed functions

--- App.js
+++ App.js
@@ -3,20 +3,25 @@ import { StyleSheet, Text, TextInput, View, Button } from "react-native";
 
 export default function App() {
   const [outputText, setOutputText] = useState("BOTTENPOP");
+  const [enteredGoal, setEnteredGoal] = useState("");
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    console.log(enteredGoal);
+  };
   return (
-    <View style={styles}>
-      <View
-        style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          alignItems: "center",
-        }}
-      >
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
         <TextInput
           placeholder="course goal"
           style={{ borderBottomColor: "black", borderBottomWidth: 1 }}
+          onChangeText={{ goalInputHandler }}
+          value={enteredGoal}
         />
-        <Button title="ADD" />
+        <Button title="ADD" onPress={addGoalHandler} />
       </View>
       <View></View>
       <Text>{outputText}</Text>
@@ -31,5 +36,10 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
   },
 });
